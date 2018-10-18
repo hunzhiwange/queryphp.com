2f735dbdb5e94e546474291c074bac4a0b5f2340
Xiangmin Liu <635750556@qq.com>
2018-10-19 07:46:49 +0800

    remove key

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,8 +26,8 @@ module.exports = {
     docsDir: 'docs',
     // #697 Provided by the official algolia team.
     // algolia: {
-    //   apiKey: '3a539aab83105f01761a137c61004d85',
-    //   indexName: 'vuepress'
+    //   apiKey: '',
+    //   indexName: ''
     // },
     locales: {
         '/': {
