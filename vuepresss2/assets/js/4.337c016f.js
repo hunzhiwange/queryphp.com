f61e9c22592c778ce09b2ca28453b6b305033fa9
Xiangmin Liu <635750556@qq.com>
2018-10-19 08:01:05 +0800

    deploy

---
+++ vuepresss2/assets/js/4.337c016f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{198:function(t,e,n){"use strict";n.r(e);var s=n(21),o=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("p",{staticClass:"demo"},[this._v("This is another component")])},[],!1,null,null,null);o.options.__file="OtherComponent.vue";e.default=o.exports}}]);
