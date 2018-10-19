981a41d459e569e4c436e8b3825e64d8cd3b8aca
Xiangmin Liu <635750556@qq.com>
2018-10-19 08:08:01 +0800

    deploy

--- init.sh
+++ init.sh
@@ -6,24 +6,19 @@ set -e
 # 生成静态文件
 npm run build
 
-# 拉取 git 仓库
-if [ test -d 文件夹名称 ] ; then
-echo "文件夹存在！"
-else
-echo "文件夹不存在！"
-fi
-
 # 进入生成的文件夹
 cd vuepress
 
 # 如果是发布到自定义域名
 echo 'www.queryphp.com' > CNAME
 
-#git init
+git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'first commit'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push git@github.com:hunzhiwange/queryphp.com.git gh-pages
+git -f push git@github.com:hunzhiwange/queryphp.com.git master:gh-pages
 
 cd -
+
+git clone -b gh-pages git@github.com:hunzhiwange/queryphp.com.git .deploy
