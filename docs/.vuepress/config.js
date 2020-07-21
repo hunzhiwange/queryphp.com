c76d1401d7244edba282c887c6c2b4a4b98acb6d
XiangMin Liu <635750556@qq.com>
2020-07-21 11:19:26 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,6 +23,8 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
