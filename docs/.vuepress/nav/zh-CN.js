962a1eaa2e8bed6d24da50ab92f856b98085c70d
XiangMin Liu <635750556@qq.com>
2020-07-25 00:21:08 +0800

    更新文档

--- docs/.vuepress/nav/zh-CN.js
+++ docs/.vuepress/nav/zh-CN.js
@@ -1,7 +1,61 @@
 module.exports = [
   {
     text: '开发文档',
-    link: '/zh-CN/docs/',
+    ariaLabel: '开发文档',
+    items: [
+      {
+        text: '前言',
+        link: '/zh-CN/docs/'
+      },
+      {
+        text: '入门',
+        link: '/zh-CN/docs/started/'
+      },
+      {
+        text: '指南',
+        link: '/zh-CN/docs/guide/'
+      },
+      {
+        text: '架构',
+        link: '/zh-CN/docs/architecture/'
+      },
+      {
+        text: '路由',
+        link: '/zh-CN/docs/router/'
+      },
+      {
+        text: '模板',
+        link: '/zh-CN/docs/template/'
+      },
+      {
+        text: '数据库',
+        link: '/zh-CN/docs/database/'
+      },
+      {
+        text: 'ORM',
+        link: '/zh-CN/docs/orm/'
+      },
+      {
+        text: '验证器',
+        link: '/zh-CN/docs/validate/'
+      },
+      {
+        text: 'Swoole',
+        link: '/zh-CN/docs/protocol/coroutine'
+      },
+      {
+        text: '组件',
+        link: '/zh-CN/docs/component/support/str'
+      },
+      {
+        text: '测试',
+        link: '/zh-CN/docs/test/'
+      },
+      {
+        text: '开发者',
+        link: '/zh-CN/docs/developer/'
+      },
+    ],
   },
   {
     text: 'API 文档',
--- docs/.vuepress/nav/zh-TW.js
+++ docs/.vuepress/nav/zh-TW.js
@@ -1,18 +1,72 @@
 module.exports = [
   {
-    text: '开发文档',
-    link: '/zh-TW/docs/',
+    text: '開發檔案',
+    ariaLabel: '開發檔案',
+    items: [
+      {
+        text: '前言',
+        link: '/zh-TW/docs/'
+      },
+      {
+        text: '入門',
+        link: '/zh-TW/docs/started/'
+      },
+      {
+        text: '指南',
+        link: '/zh-TW/docs/guide/'
+      },
+      {
+        text: '架構',
+        link: '/zh-TW/docs/architecture/'
+      },
+      {
+        text: '路由',
+        link: '/zh-TW/docs/router/'
+      },
+      {
+        text: '模板',
+        link: '/zh-TW/docs/template/'
+      },
+      {
+        text: '資料庫',
+        link: '/zh-TW/docs/database/'
+      },
+      {
+        text: 'ORM',
+        link: '/zh-TW/docs/orm/'
+      },
+      {
+        text: '驗證器',
+        link: '/zh-TW/docs/validate/'
+      },
+      {
+        text: 'Swoole',
+        link: '/zh-TW/docs/protocol/coroutine'
+      },
+      {
+        text: '組件',
+        link: '/zh-TW/docs/component/support/str'
+      },
+      {
+        text: '測試',
+        link: '/zh-TW/docs/test/'
+      },
+      {
+        text: '開發者',
+        link: '/zh-TW/docs/developer/'
+      },
+    ],
   },
   {
-    text: 'API 文档',
+    text: 'API 檔案',
     link: 'https://api.queryphp.com/',
   },
   {
-    text: '社区',
+    text: '社區',
     link: '/zh-TW/ecosystem/',
   },
   {
-    text: '关于我们',
+    text: '關於我們',
     link: '/zh-TW/about/'
   }
 ]
